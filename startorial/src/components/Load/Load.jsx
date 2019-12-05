6cdab94595f9bcb350b4a3e5a085393eace4da81
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-12-06 01:03:09 +0530

    creating the load component

---
+++ startorial/src/components/Load/Load.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Load.css'
+const Load = () => {
+    return (
+        <div className="loading">
+           <b>Loading...</b>
+        </div>
+    )
+}
+
+export default Load
