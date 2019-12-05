3abd9c93e720bd3db2f1544ca6842e71637653ed
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-12-05 21:35:48 +0530

    setting the environment

---
+++ startorial/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Searchbar from './components/searchbar/searchbar.component'
+import Search from './components/search/search.component'
+function App() {
+  return (
+    <div className="App">
+       <Search />
+    </div>
+  );
+}
+
+export default App;
