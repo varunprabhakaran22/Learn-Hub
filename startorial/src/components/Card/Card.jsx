5973c602abd0ed9884b2f1ebf4b3904dc1f7499f
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-12-06 04:19:34 +0530

    Adding the comments and checking the indendation

--- startorial/src/components/Card/Card.jsx
+++ startorial/src/components/Card/Card.jsx
@@ -1,8 +1,11 @@
+//Importing the required packages for this component
 import React from 'react'
 import './Card.css'
 export const Card = (props) => {
     let listItem = props.list
     return (
+
+        //Rendering the attribute along with the value 
         <div className="cardItem">
             <div className ="card" style={{width : "18rem"}}>
                 <div className ="card-body">
@@ -12,9 +15,8 @@ export const Card = (props) => {
                     <p className ="providerName"><b>Category </b>:<span> {listItem["Parent Subject"]?listItem["Parent Subject"]:'N/A'}</span></p>
                     <p className ="providerName"><b>Next Session</b>:<span> {listItem["Next Session Date"]?listItem["Next Session Date"]:'N/A'}</span></p>
                     <p className ="providerName"><b>Length </b>:<span> {listItem["Length"]?listItem["Length"]:'N/A'}</span></p>
-                    <a href={listItem["Url"]} className ="btn btn-secondary mr-3"  > Apply </a> 
-                    <a href={listItem["Video(Url)"]} className ="btn btn-primary" >Watch Now</a>
-                
+                    <a href={listItem["Url"]} className ="btn btn-secondary mr-3" > Apply </a> 
+                    <a href={listItem["Video(Url)"]} className ="btn btn-primary" > Watch Now</a>
                 </div>
             </div>
         </div>
