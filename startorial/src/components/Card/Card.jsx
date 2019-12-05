e4e7b7cc3de43a180475c8b00e807aae05167c0d
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-12-06 03:08:49 +0530

    Adding one more button to visit the course provider link

--- startorial/src/components/Card/Card.jsx
+++ startorial/src/components/Card/Card.jsx
@@ -6,13 +6,14 @@ export const Card = (props) => {
         <div className="cardItem">
             <div className ="card" style={{width : "18rem"}}>
                 <div className ="card-body">
-                    <h5 className ="card-title">{listItem["Course Name"]}</h5>
+                    <h5 className ="card-title">{listItem["Course Name"].toUpperCase()}</h5>
                     <p className ="providerName"><b>Provider </b>:<span> {listItem["Provider"]?listItem["Provider"]:'N/A'}</span></p>
-                    <p className ="providerName"><b>Universities/Institutions </b>:<span> {listItem["Universities/Institutions"]?listItem["Universities/Institutions"]:'N/A'}</span></p>
-                    <p className ="providerName"><b>Parent Subject </b>:<span> {listItem["ParentSubject"]?listItem["ParentSubject"]:'N/A'}</span></p>
-                    <p className ="providerName"><b>Next Session Date </b>:<span> {listItem["Next Session Date"]?listItem["Next Session Date"]:'N/A'}</span></p>
+                    <p className ="providerName"><b>University</b>:<span> {listItem["Universities/Institutions"]?listItem["Universities/Institutions"]:'N/A'}</span></p>
+                    <p className ="providerName"><b>Category </b>:<span> {listItem["Parent Subject"]?listItem["Parent Subject"]:'N/A'}</span></p>
+                    <p className ="providerName"><b>Next Session</b>:<span> {listItem["Next Session Date"]?listItem["Next Session Date"]:'N/A'}</span></p>
                     <p className ="providerName"><b>Length </b>:<span> {listItem["Length"]?listItem["Length"]:'N/A'}</span></p>
                     <a href={listItem["Video(Url)"]} className ="btn btn-primary">Watch Now</a>
+                    <a href={listItem["Url"]} className ="btn btn-primary"> Visit </a>
                 </div>
             </div>
         </div>
