96cdabfe8a2c43eb93f3b2f7507154349fa35e7c
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-12-06 06:08:54 +0530

    completed

--- startorial/src/components/Sort/Sort.jsx
+++ startorial/src/components/Sort/Sort.jsx
@@ -1,27 +1,31 @@
 //Importing the required packages for this component
 import React from 'react'
+import './Sort.css'
 
 //Functional component 
 const Sorting = ( props )=>{
     const { setResult } = props
     let listItem = props.data
 
-    //Method to sort the value based on length
+    //Method to sort the value based on length in Ascending Order
     let sortAsc =()=>{
         let sortedData = listItem.sort((a, b) =>{ return a.Length - b.Length })
         listItem = sortedData 
         setResult(listItem)
     }
 
+    //Method to sort the value in Descending order
     let sortDesc =()=>{
         let sortedData = listItem.sort((a, b) =>{ return b.Length - a.Length })
         listItem = sortedData 
         setResult(listItem)
     }
+
+    
     return(
-        <div> Sort
-            <button className="button" onClick={sortAsc}> <i className="fa fa-sort-asc" ></i></button>   
-            <button className="button" onClick={sortDesc}> <i className="fa fa-sort-desc" > </i></button>
+        <div className = "sorting"> Sort
+            <button className="button give-space"   onClick={sortAsc}> <i className="fa fa-sort-asc" ></i></button>   
+            <button className="button give-space"   onClick={sortDesc}> <i className="fa fa-sort-desc" > </i></button>
         </div>
     )
 }
