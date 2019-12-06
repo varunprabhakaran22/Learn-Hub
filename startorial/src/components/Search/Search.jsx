275edc6869fdf8457f16e155e4679f0b70dc2b92
varunprabhakaran22 <50875768+varunprabhakaran22@users.noreply.github.com>
2019-12-06 06:00:05 +0530

    Creating the new component and pass the value to component as props

--- startorial/src/components/Search/Search.jsx
+++ startorial/src/components/Search/Search.jsx
@@ -5,6 +5,7 @@ import axios from 'axios'
 import Card from '../Card/Card.jsx'
 import Detail from '../Details/Details.jsx'
 import Load from '../Load/Load.jsx'
+import Sort from "../Sort/Sort"
 import './Search.css'
 
 
@@ -16,6 +17,7 @@ class Search extends Component{
         this.state = {
             data : [],
             result :[],
+            sorting:false,
             loading:false
         }
     }
@@ -54,16 +56,24 @@ class Search extends Component{
         })
     }
 
+    setSorting = (sorting) => {
+        this.setState({
+            sorting
+        })
+    }
+
+
     //Taking the props value out using destructive method
     //If filter is empty diaplaying all the values in homepage else display only the filtered result
     render(){
-        const { type,data,result,loading } = this.state
+        const { type,data,result,loading,sorting } = this.state
         let listItems = result.length > 0 ? result : data
         let displayList = listItems.map(list => <Card key={list["Course Id"]} list={list}/>)
         return (
             <div>
                 <Input data={ data } setResult={this.setResult} setLoading={this.setLoading} />
                 <Detail total={data.length} found={displayList.length}/>
+                <Sort data ={listItems}  setResult= {this.setResult}/>
                 {
                     loading ? <Load />
                     :
